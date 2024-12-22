209e0ea156d8c50a1ac577e76fdbab2702081e22
TrigCodes <trigasus14@gmail.com>
2024-12-22 11:18:42 -0800

    modified search logic & cleanup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,34 @@
 import React, { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css";
 import Box from "./Box";
 
 const App = () => {
-  const [count, setCount] = useState(0);
   const [searchQuery, setSearchQuery] = useState("");
 
-  const handleSearch = (e) => {
-    setSearchQuery(e.target.value);
-    console.log("Searching for:", e.target.value); // Optional: Handle search logic
+  const handleSearch = () => {
+    console.log(`Searching for: ${searchQuery}`); // Optional: Handle search logic
   };
 
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
       <div className="testButton">
         <button>Wishlist</button>
         <button>Collection</button>
         <button>Login</button>
       </div>
+
       <div className="searchBar">
         <input
           type="text"
           placeholder="Search..."
           value={searchQuery}
-          onChange={handleSearch}
+          onChange={(event) => setSearchQuery(event.target.value)}
         />
+        <button onClick={handleSearch}>
+          Search
+        </button>
       </div>
+      
       <div className="grid">
         <Box image="https://static.wikia.nocookie.net/gunplabuilders/images/f/fa/RG-RX-78-2-Gundam-(2.0)-box.jpg" />
         <Box content="Box 2" />
