4eb903ba2c4e07784471cb6f324e9047af144a67
dragon123al <alin100@calpoly.edu>
2024-12-23 12:08:51 -0800

    added more kit image / name examples

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,24 +24,40 @@ const App = () => {
           value={searchQuery}
           onChange={(event) => setSearchQuery(event.target.value)}
         />
-        <button onClick={handleSearch}>
-          Search
-        </button>
+        <button onClick={handleSearch}>Search</button>
       </div>
-      
+
       <div className="grid">
-        <Box image="https://static.wikia.nocookie.net/gunplabuilders/images/f/fa/RG-RX-78-2-Gundam-(2.0)-box.jpg" />
-        <Box content="Box 2" />
-        <Box content="Box 3" />
-        <Box content="Box 4" />
-        <Box content="Box 5" />
-        <Box content="Box 6" />
-        <Box content="Box 7" />
-        <Box content="Box 8" />
-        <Box content="Box 9" />
-        <Box content="Box 10" />
-        <Box content="Box 11" />
-        <Box content="Box 12" />
+        <Box
+          image="https://static.wikia.nocookie.net/gunplabuilders/images/f/fa/RG-RX-78-2-Gundam-(2.0)-box.jpg"
+          content="RG 1/144 RX-78-2 Gundam [Ver. 2.0]"
+        />
+        <Box
+          image="https://cdn.shopify.com/s/files/1/2786/5582/products/191_3419_s_to0m866o5x5p8duhwc8b2f6wjvxz_clipped_rev_1.png"
+          content="MG 1/100 ASW-G-08 Gundam Barbatos"
+        />
+        <Box
+          image="https://static.wikia.nocookie.net/gunplabuilders/images/2/2e/MG-Gundam-NT-1-%28Ver.2.0%29-box.jpg"
+          content="MG 1/100 RX-78NT-1 Gundam NT-1 Alex Ver 2.0"
+        />
+        <Box
+          image="https://static.wikia.nocookie.net/gunplabuilders/images/b/b7/PG-Unicorn-Gundam-box.jpg"
+          content="PG 1/60 Unicorn Gundam"
+        />
+        <Box
+          image="https://cdn.shopify.com/s/files/1/2786/5582/files/boxart_45828b86-66cc-4905-b463-906b778f84bf.jpg"
+          content="HG 1/144 Gundam Calibarn"
+        />
+        <Box
+          image="https://ae01.alicdn.com/kf/Sa5c9f138c7ac4292bd149bba57923c71l.jpg"
+          content="SD Gundam Aerial EX-Standard Gunpla"
+        />
+        <Box image="" content="Box 7" />
+        <Box image="" content="Box 8" />
+        <Box image="" content="Box 9" />
+        <Box image="" content="Box 10" />
+        <Box image="" content="Box 11" />
+        <Box image="" content="Box 12" />
       </div>
     </>
   );
