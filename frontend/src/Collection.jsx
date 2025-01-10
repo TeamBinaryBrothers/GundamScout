9fcbd04b3b1d33e2418e448df636837f423e822a
dragon123al <alin100@calpoly.edu>
2025-01-09 20:13:13 -0800

    collection header added

--- frontend/src/Collection.jsx
+++ frontend/src/Collection.jsx
@@ -4,10 +4,13 @@ import "./Collection.css";
 
 const Collection = () => {
   return (
-    <div className="collection-button-container">
-      <Link to="/" className="back-button">
-        <button>Back to Home Page</button>
-      </Link>
+    <div>
+      <h1 className="collection-header">Collection</h1>
+      <div className="collection-button-container">
+        <Link to="/" className="back-button">
+          <button>Back to Home Page</button>
+        </Link>
+      </div>
     </div>
   );
 };
